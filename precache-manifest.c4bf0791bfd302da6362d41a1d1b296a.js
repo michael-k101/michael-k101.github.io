3549528f2ece96b0a1646cb91268e0cc549fe5f0
Michael Koohang <michael.s.k1001@gmail.com>
2020-05-15 23:15:17 -0400

    Updates

---
+++ precache-manifest.c4bf0791bfd302da6362d41a1d1b296a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db68dae149057e7c81d28e3bfbe174a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f44aaa3be34c78a107c",
+    "url": "/static/css/main.c7574ecc.chunk.css"
+  },
+  {
+    "revision": "e7e007dd21383318f12b",
+    "url": "/static/js/2.d3f2bec2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3f2bec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f44aaa3be34c78a107c",
+    "url": "/static/js/main.f470a8a7.chunk.js"
+  },
+  {
+    "revision": "61d75153ebbe9a5a0c2c",
+    "url": "/static/js/runtime-main.59db02b1.js"
+  },
+  {
+    "revision": "98c100c92859baa397e097bf9923ce4c",
+    "url": "/static/media/apple.98c100c9.png"
+  },
+  {
+    "revision": "07b1552661df5c0490a092a77fc9b0ff",
+    "url": "/static/media/gt.07b15526.png"
+  },
+  {
+    "revision": "4e5f07cf15c64bd04c77a18e3051209e",
+    "url": "/static/media/logo.4e5f07cf.png"
+  },
+  {
+    "revision": "3ebb8110031aba3053e549dfe074ab8e",
+    "url": "/static/media/profile-pic.3ebb8110.jpeg"
+  },
+  {
+    "revision": "93944c611322b5004b876e66f31fce76",
+    "url": "/static/media/stanford.93944c61.png"
+  }
+]);
